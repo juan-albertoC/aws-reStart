c434d02db6ac5da9604cb7bfc3aa6b3294df540a
Juan Alberto Cuevas Juarez <136052531+juan-albertoC@users.noreply.github.com>
2024-01-24 21:31:49 -0600

    Variable n numero maximo de primos, uso de ciclo for con variables iniciales j = 2, ya que uno no primo, afirma con variable esPrimo que todos son primos al hacer True

--- retoPython/reto.py
+++ retoPython/reto.py
@@ -1,9 +1,12 @@
-print('Hello, World!')
+#Desafio de AWS Juan Alberto Cuevas Juarez
+#Programa muestra numeros primos de 1 hasta 250, combina condicionales if-else, bucles for-while
 
-print('Bienvenidos al reto de python de Aws-ReStart')
+#crete directorio de trabajo y archivos py, txt 
 
-print('Imprime un rango de numeros entre 250 ')
+n = 250                                   # Opcion 1 variable n: numero maximo de primos
+#n = int(input("Indica numero maximo de primos")) # Opcion 2 Entrada por teclado con la funcion input para diferentes numeros maximos de primos
 
-for i in range(250):
-     print(i)
 
+for i in range(2, n+1):                   #En ciclo for variable i de un rango desde 2 hasta n + 1
+       j = 2                              #comienza aqui j en 2
+       esPrimo = True                     #variable booleano en True
